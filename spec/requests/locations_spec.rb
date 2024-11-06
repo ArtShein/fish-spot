2b4ce0cb54223267553279731966dcd3b9f9b3c7
ArtShein <art.shein.2002@gmail.com>
2024-11-06 12:48:57 +0200

    Tailwind and rubocop setup

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -13,7 +13,6 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/locations", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # Location. As you add validations to Location, be sure to
   # adjust the attributes here as well.
@@ -77,12 +76,11 @@ RSpec.describe "/locations", type: :request do
         }.to change(Location, :count).by(0)
       end
 
-    
+
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post locations_url, params: { location: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
     end
   end
 
@@ -108,13 +106,11 @@ RSpec.describe "/locations", type: :request do
     end
 
     context "with invalid parameters" do
-    
       it "renders a response with 422 status (i.e. to display the 'edit' template)" do
         location = Location.create! valid_attributes
         patch location_url(location), params: { location: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
     end
   end
 
--- spec/views/trips/new.html.erb_spec.rb
+++ spec/views/trips/new.html.erb_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe "trips/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", trips_path, "post" do
-
       assert_select "input[name=?]", "trip[name]"
     end
   end
